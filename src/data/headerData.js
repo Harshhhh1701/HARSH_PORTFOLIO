e6125ab685ad3d7a88383a48018ab4be3381ff3a
Harsh <valambeharsh1701@gmail.com>
2023-07-19 02:49:29 +0530

    updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Harsh Valambe',
     title: "Full Stack Web Developer",
     desciption:"I am passionate Full Stack web developer having an experience of web applications with JavaScript, NodeJS, React.js,React Native,TypeSript and Next.js.",
-    image: 'https://media.licdn.com/dms/image/D4D35AQFba9t5wCDKnw/profile-framedphoto-shrink_400_400/0/1688072347938?e=1689012000&v=beta&t=7bSBWOcLm5h0zx0iQbRPXxdQg6DiNoZSPtiYxWc9OJM',
+    image: 'https://media.licdn.com/dms/image/D4D35AQFba9t5wCDKnw/profile-framedphoto-shrink_400_400/0/1688072347938?e=1690322400&v=beta&t=tCgmEbzgBjtlMVy6osv-5IATPfRP1mEZtX7YTxvyHXo',
     resumePdf: resume
 }
